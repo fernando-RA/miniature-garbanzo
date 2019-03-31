b6345c02476e2e8e64472e391aecfe8283d1f5cb
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-03-31 02:29:03 -0300

    Creating state for todos

--- miniature-garbanzo/src/App.js
+++ miniature-garbanzo/src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      message: 'Hello World!',
+      newTodo: ''
+    };
+  }
+
+  formSubmitted(event){
+    event.preventDefault();
+    console.log(this.state.newTodo);
+  }
+
+  newTodoChanged(event){
+    this.setState({
+      newTodo: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h3>{this.state.message}</h3>
+        <form onSubmit={this.formSubmitted.bind(this)}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input 
+            onChange={(event) => this.newTodoChanged(event)} 
+            id="newTodo" 
+            name="newTodo">
+          </input>
+          <button type="submit">Add Todo</button>
+        </form>
       </div>
     );
   }
