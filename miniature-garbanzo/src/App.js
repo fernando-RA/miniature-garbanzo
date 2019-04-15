ba69a372b08defb41160cd68f9c6c3eb3af2e575
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-04-15 15:20:17 -0300

    Refactor style todo done

--- miniature-garbanzo/src/App.js
+++ miniature-garbanzo/src/App.js
@@ -63,9 +63,7 @@ class App extends Component {
           {this.state.todos.map((todo, index) => {
               return (<li key={todo.title}>
                   <input onChange={(event) => this.toggleTodoDone(event, index)} type="checkbox"/>
-                  <span style={{
-                    textDecoration:todo.done ? 'line-through' : 'inherit'
-                    }}>{todo.title}</span>
+                  <span className={todo.done ? 'done' : ''}>{todo.title}</span>
           </li>)})}
         </ul>
       </div>
